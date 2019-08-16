504c1fb06bc92c12112c89616eb436e73881ae89
hotman1986 <hongxiang.tang@studenti.unicam.it>
2019-08-17 01:26:01 +0200

    Add files via upload

---
+++ learn python3 in a hardway/ex13.py
@@ -0,0 +1,18 @@
+#ex13
+from sys import argv
+#read the WYSS section for how to run this
+
+script,first,second,third = argv
+
+print("The script is called:",script)
+print("Your first variable is:",first)
+print("Your second variable is:",second)
+print("Your third variable is:",third)
+script = input("script")
+first = input("first")
+second = input("second")
+third = input("third")
+print("The script is called:",script)
+print("Your first variable is:",first)
+print("Your second variable is:",second)
+print("Your third variable is:",third)
